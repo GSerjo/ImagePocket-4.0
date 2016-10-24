31870783bd62b29ca327d9f10e453b43b107c9d0
GSerjo <sedot8@gmail.com>
2016-10-23 23:50:25 -0400

    * side menu initial verions

--- ImagePocket/ImagePocket/AppDelegate.swift
+++ ImagePocket/ImagePocket/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SideMenuController
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
---
+++ ImagePocket/ImagePocket/CustomSideMenuController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomSideMenuController.swift
+//  ImagePocket
+//
+//  Created by Serjo on 23/10/16.
+//  Copyright © 2016 Serjo. All rights reserved.
+//
+
+import Foundation
+import SideMenuController
+
+final class CustomSideMenuController : SideMenuController {
+    
+    required init?(coder aDecoder: NSCoder) {
+        SideMenuController.preferences.drawing.menuButtonImage = UIImage(named: "menu")
+        SideMenuController.preferences.drawing.sidePanelPosition = .underCenterPanelLeft
+        SideMenuController.preferences.drawing.sidePanelWidth = 300
+        SideMenuController.preferences.drawing.centerPanelShadow = true
+        SideMenuController.preferences.animating.statusBarBehaviour = .horizontalPan
+        SideMenuController.preferences.animating.transitionAnimator = FadeAnimator.self
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        performSegue(withIdentifier: "showCenterController", sender: nil)
+        performSegue(withIdentifier: "containSideMenu", sender: nil)
+    }
+    
+}
