78cd190a02888290ae573215bf5b8c1ab10c86a6
GSerjo <sedot8@gmail.com>
2017-12-09 20:58:23 -0500

    bottom/up tolbars

--- ImagePocket/ImagePocket/MainViewController.swift
+++ ImagePocket/ImagePocket/MainViewController.swift
@@ -9,13 +9,30 @@
 import UIKit
 import Photos
 
-class MainViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, GalleryItemsDataSource {
+
+class MainViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, GalleryItemsDataSource, UISearchBarDelegate {
 
     private var _imageCache: ImageCache!
     private var _selectedTag = TagEntity.all
     private let _settings = Settings.instance
     private var _filteredImages = [ImageEntity]()
     private let _imageManager = PHCachingImageManager()
+    private var _viewMode = ViewMode.read
+    private var _selectedImages = [String: ImageEntity]()
+    private var _searchText = String.empty
+    private var _pendingSearchRequest: DispatchWorkItem?
+    
+    @IBOutlet var _btSelect: UIBarButtonItem!
+    @IBOutlet var _btSearch: UIBarButtonItem!
+    @IBOutlet var _btCancel: UIBarButtonItem!
+    @IBOutlet var _btTag: UIBarButtonItem!
+    @IBOutlet weak var _btTrash: UIBarButtonItem!
+    @IBOutlet weak var _btShare: UIBarButtonItem!
+    private var _searchBar = UISearchBar()
+    
+    private var isAnyImagesSelected: Bool {
+        return _selectedImages.isEmpty == false
+    }
     
     private lazy var _requestPreviewImageOptions: PHImageRequestOptions = {
         let result = PHImageRequestOptions()
@@ -36,7 +53,44 @@ class MainViewController: UICollectionViewController, UICollectionViewDelegateFl
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
+    @IBAction func onSearchClicked(_ sender: Any) {
+        setSearchMode()
+    }
+    
+    @IBAction func onSelectClicked(_ sender: Any) {
+        setSelectMode()
+    }
+    
+    @IBAction func onCancelClicked(_ sender: Any) {
+        let anyImagesSelected = isAnyImagesSelected
+        setReadMode()
+        if anyImagesSelected {
+            reloadData()
+        }
+    }
+    
+    @IBAction func onTagClicked(_ sender: Any) {
+    }
+    
+    @IBAction func onTrashClicked(_ sender: Any) {
+        let ids = Array(_selectedImages.keys)
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.deleteAssets(self._imageCache[ids] as NSArray)
+        }) { (completed, _) in
+            if completed {
+                self._imageCache.remove(localIdentifiers: ids)
+            }
+            DispatchQueue.main.sync {
+                self.setReadMode()
+                self.filterImagesAndReloadAsync(by: self._selectedTag)
+            }
+        }
+    }
+    
+    @IBAction func onShareClicked(_ sender: Any) {
+    }
+    
     // CollectionView
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return _filteredImages.count
@@ -51,6 +105,13 @@ class MainViewController: UICollectionViewController, UICollectionViewDelegateFl
         }
         
         cell.representedAssetIdentifier = asset.localIdentifier
+        
+        if _selectedImages.isEmpty == false && _selectedImages.keys.contains(cell.representedAssetIdentifier) {
+            cell.selectCell()
+        }else {
+            cell.deselectCell()
+        }
+        
         _imageManager.requestImage(for: asset, targetSize: imagePreviewCellSize(), contentMode: .aspectFill, options: _requestPreviewImageOptions, resultHandler: { image, _ in
             if let image = image, cell.representedAssetIdentifier == asset.localIdentifier {
                 cell.thumbnailImage = image
@@ -65,13 +126,31 @@ class MainViewController: UICollectionViewController, UICollectionViewDelegateFl
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let cell = collectionView.cellForItem(at: indexPath) as! ImagePreviewCell
-//        let image = _filteredImages[indexPath.item]
-//        let photos = _imageCache[_filteredImages.map{$0.localIdentifier}].map{SKPhoto(asset: $0)}
-        
-        let galleryViewController = GalleryViewController(startIndex: indexPath.item, itemsDataSource: self, configuration: galleryConfiguration())
-        
-        present(galleryViewController, animated: false, completion: nil)
+        if _viewMode == .select {
+            let cell = collectionView.cellForItem(at: indexPath) as! ImagePreviewCell
+            let image = _filteredImages[indexPath.item]
+            
+            
+            if _selectedImages.keys.contains(image.localIdentifier) {
+                
+                _selectedImages.removeValue(forKey: image.localIdentifier)
+                cell.deselectCell()
+            }
+            else {
+                _selectedImages[image.localIdentifier] = image
+                cell.selectCell()
+            }
+            
+            onSelectedImageChanged()
+            
+        } else {
+            if _viewMode == .search {
+                setReadMode()
+            }
+            
+            let galleryViewController = GalleryViewController(startIndex: indexPath.item, itemsDataSource: self, configuration: galleryConfiguration())
+            present(galleryViewController, animated: false, completion: nil)
+        }
     }
     
     public func loadUnderlyingImageAndNotify(_asset: PHAsset) -> Void {
@@ -95,6 +174,192 @@ class MainViewController: UICollectionViewController, UICollectionViewDelegateFl
         })
     }
     
+    // Switch Mode
+    private enum ViewMode {
+        case read, select, search
+    }
+    
+    private func setSelectMode() -> Void {
+        _viewMode = .select
+        title = AppTitle.selectImages
+        
+         navigationItem.rightBarButtonItems?.removeAll()
+        navigationItem.rightBarButtonItems?.append(_btCancel)
+    }
+    
+    private func setReadMode() -> Void {
+        _viewMode = .read
+        navigationItem.titleView = nil
+        title = AppTitle.root
+        
+        _selectedImages = [String: ImageEntity]()
+        onSelectedImageChanged()
+
+        
+        navigationItem.rightBarButtonItems?.removeAll()
+        
+        if navigationItem.rightBarButtonItems == nil {
+            navigationItem.rightBarButtonItems = [_btSelect, _btSearch]
+        } else{
+            navigationItem.rightBarButtonItems!.append(_btSelect)
+            navigationItem.rightBarButtonItems!.append(_btSearch)
+        }
+    }
+    
+    private func onSelectedImageChanged() -> Void {
+        _btTrash.isEnabled = isAnyImagesSelected
+        _btShare.isEnabled = isAnyImagesSelected
+    }
+    
+    private func setSearchMode() -> Void {
+        _viewMode = .search
+        
+        navigationItem.leftBarButtonItems = nil
+        navigationItem.rightBarButtonItems = nil
+        
+        configureSearchCancelButtonIfPad()
+        navigationItem.titleView = _searchBar
+        
+        _searchBar.sizeToFit()
+        _searchBar.endEditing(true)
+        _searchBar.becomeFirstResponder()
+    }
+    
+    private func configureSearchCancelButtonIfPad() -> Void {
+        if UIDevice.current.userInterfaceIdiom != .pad {
+            return
+        }
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(onCancelSearch))
+        navigationItem.rightBarButtonItem = cancelButton
+    }
+
+    private func imagePreviewCellSize() -> CGSize {
+        let cellWidth = _collectionView.frame.width / 3 - 8
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+    
+    private func configure() -> Void {
+        title = AppTitle.root
+        
+        _searchBar.showsCancelButton = true
+        _searchBar.delegate = self
+        _searchBar.placeholder = "Search Photos"
+        _searchBar.returnKeyType = UIReturnKeyType.done
+        
+        onSelectedImageChanged()
+    }
+    
+    private func reloadDataAsync() {
+        DispatchQueue.main.async { [unowned self] in
+            self.reloadData()
+        }
+    }
+    
+    private func reloadData() {
+        _collectionView.reloadData()
+    }
+    
+    private func startApp(){
+        if(PHPhotoLibrary.authorizationStatus() == .authorized){
+            startAppCore()
+        }
+        else{
+            PHPhotoLibrary.requestAuthorization(requestAuthorizationHandler)
+        }
+    }
+    
+    private func startAppCore(){
+        _selectedTag = _settings.getTag()
+        
+        _imageCache = ImageCache.instance
+        _imageCache.startAsync(onComplete: {
+            self.filterImagesAsync(by: self._selectedTag, onComplete: self.reloadDataAsync)
+        })
+    }
+    
+   
+    private func filterImagesAsync(by tag: TagEntity?, onComplete: @escaping () -> Void = {}) -> Void {
+        if let tagEntity = tag {
+            _selectedTag = tagEntity
+            _imageCache.getImagesAsync(tag: tagEntity, onComplete: { images in
+                self._filteredImages = images
+                onComplete()
+            })
+        }
+    }
+    
+    private func filterImagesAndReloadAsync(by tag: TagEntity?) -> Void {
+        if let tagEntity = tag {
+            filterImagesAsync(by: tagEntity, onComplete: reloadDataAsync)
+        }
+    }
+       
+    private func requestAuthorizationHandler(_ status: PHAuthorizationStatus){
+        DispatchQueue.main.sync {
+            if(status == .authorized){
+                startAppCore()
+            }
+            else {
+                
+                let alertController = UIAlertController(title: "Warning", message: "The Photo permission was not authorized. Please enable it in Settings to continue", preferredStyle: .alert)
+                let settingsAction = UIAlertAction(title: "Open Settings", style: .default, handler: {_ in
+                    if let appSettings = URL(string: UIApplicationOpenSettingsURLString){
+                        UIApplication.shared.open(appSettings, options: [:], completionHandler: nil)
+                    }
+                })
+                
+                alertController.addAction(settingsAction)
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                alertController.addAction(cancelAction)
+                present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    // UISearchBarDelegate
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        onCancelSearch()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchImages(by: searchText)
+    }
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        _searchBar.text = nil
+        _searchBar.endEditing(false)
+    }
+    
+    @objc private func onCancelSearch() -> Void {
+        _searchBar.text = nil
+        setReadMode()
+        filterImagesAndReloadAsync(by: _selectedTag)
+        _searchText = String.empty
+    }
+    
+    private func searchImages(by searchText: String) -> Void {
+        _searchText = searchText
+        if searchText.isEmpty() {
+            return
+        }
+        _pendingSearchRequest?.cancel()
+        let searchRequest = DispatchWorkItem{ [unowned self] in
+            self._filteredImages = self._imageCache.search(text: self._searchText)
+            self.reloadDataAsync()
+        }
+        
+        _pendingSearchRequest = searchRequest
+        DispatchQueue.global().asyncAfter(deadline: .now() + .milliseconds(200), execute: searchRequest)
+    }
+    
+   
     // Gallery
     func itemCount() -> Int {
         return _filteredImages.count
@@ -160,77 +425,5 @@ class MainViewController: UICollectionViewController, UICollectionViewDelegateFl
             GalleryConfigurationItem.displacementKeepOriginalInPlace(false),
             GalleryConfigurationItem.displacementInsetMargin(50)
         ]
-    }
-    
-    
-    private func imagePreviewCellSize() -> CGSize {
-        let cellWidth = _collectionView.frame.width / 3 - 8
-        return CGSize(width: cellWidth, height: cellWidth)
-    }
-    
-    private func configure() -> Void {
-        title = AppTitle.root
-    }
-    
-    private func startApp(){
-        if(PHPhotoLibrary.authorizationStatus() == .authorized){
-            startAppCore()
-        }
-        else{
-            PHPhotoLibrary.requestAuthorization(requestAuthorizationHandler)
-        }
-    }
-    
-    private func startAppCore(){
-        _selectedTag = _settings.getTag()
-        
-        _imageCache = ImageCache.instance
-        _imageCache.startAsync(onComplete: {
-            self.filterImagesAsync(by: self._selectedTag, onComplete: self.reloadDataAsync)
-        })
-    }
-    
-   
-    private func filterImagesAsync(by tag: TagEntity?, onComplete: @escaping () -> Void = {}) -> Void {
-        if let tagEntity = tag {
-            _selectedTag = tagEntity
-            _imageCache.getImagesAsync(tag: tagEntity, onComplete: { images in
-                self._filteredImages = images
-                onComplete()
-            })
-        }
-    }
-    
-    private func reloadDataAsync() {
-        DispatchQueue.main.async { [unowned self] in
-            self.reloadData()
-        }
-    }
-    
-    private func reloadData() {
-        _collectionView.reloadData()
-    }
-    
-    private func requestAuthorizationHandler(_ status: PHAuthorizationStatus){
-        DispatchQueue.main.sync {
-            if(status == .authorized){
-                startAppCore()
-            }
-            else {
-                
-                let alertController = UIAlertController(title: "Warning", message: "The Photo permission was not authorized. Please enable it in Settings to continue", preferredStyle: .alert)
-                let settingsAction = UIAlertAction(title: "Open Settings", style: .default, handler: {_ in
-                    if let appSettings = URL(string: UIApplicationOpenSettingsURLString){
-                        UIApplication.shared.open(appSettings, options: [:], completionHandler: nil)
-                    }
-                })
-                
-                alertController.addAction(settingsAction)
-                
-                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-                alertController.addAction(cancelAction)
-                present(alertController, animated: true, completion: nil)
-            }
-        }
     }
 }
